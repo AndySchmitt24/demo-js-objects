bb1ecd4679e2b19d09f8fc5160fbb0839a2db276
Andy <andyschmitt24@gmail.com>
2023-08-17 11:08:56 +0200

    Objecte

---
+++ demo.js
@@ -0,0 +1,63 @@
+// Neuen Type Objekt kennengelernt
+/*
+const person = {};
+console.log(typeof person);
+
+// Access Property Values und verschiedene varrianten Objekte zu deklarieren
+/and change them
+const person = {
+  name: "John",
+  age: 38,
+  job: "developer",
+};
+console.log(person);
+console.log(person.age);
+console.log(person["name"]);
+
+Scecial Fall
+const person2 = {
+    1: "test",
+    "john Doe": 
+}
+
+// Nesting of Objects
+const person = {
+  name: "john",
+  job: "developer",
+  age: 38,
+  adresse: {
+    street: "mainstreet",
+    state: "NY",
+    zipCode: 10001,
+  },
+};
+console.log(person.adresse.zipCode);
+
+const person = {
+  name: "John",
+  age: 38,
+  job: "developer",
+};
+console.log(person);
+
+Comparison of Objects
+Reference Types vs. Primite / types
+console.log(23 === 23);
+
+const person2 = {
+  name: "John",
+  age: 38,
+  job: "developer",
+};
+*/
+/*
+// Nachträglich weitere Objekte einer Person hinzufügen
+const person = {
+  name: "John",
+  age: 38,
+};
+person.job = "developer";
+
+person.adress = { street: "Mainstreet", state: "NY" };
+console.log(person);
+*/
